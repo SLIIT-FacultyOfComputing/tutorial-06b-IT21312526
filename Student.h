5133f2c6a539f638dd8f73b4782c25d083a281d8
IT21312526 <99425476+IT21312526@users.noreply.github.com>
2022-03-16 05:55:35 +0000

    completed exercise 1 and 2

--- Student.h
+++ Student.h
@@ -1,9 +1,14 @@
 class Student {
   // private section
-  //    int studentId
-  //    name <- 20 charcters
-  
+  private :
+    int studentId ;
+    char name[20] ;
+ 
   // public section
+  public:
+    void assignDetails( int pstdId , char pname[]) ;
+    void display() ;
+
   //    assignDetails() method declaration
   //    display() method declaration
 };
